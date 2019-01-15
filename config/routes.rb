b5362e91d4ac7eded99e45d1d71c8daff014218e
Bikram <you@example.com>
2019-01-15 23:49:07 +0530

    added basics of login and admin part

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
 Rails.application.routes.draw do
-  get 'acess/menu'
-  get 'acess/login'
-  get 'sections/index'
-  get 'sections/show'
-  get 'sections/new'
-  get 'sections/edit'
-  get 'sections/delete'
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
+  root 'demo#index'
+
+  get 'admin', :to => 'access#menu'
+  get 'access/menu'
+  get 'access/login'
+  post 'access/attempt_login'
+  get 'access/logout'
+
+  # get 'sections/index'
+  # get 'sections/show'
+  # get 'sections/new'
+  # get 'sections/edit'
+  # get 'sections/delete'
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
   # get 'subjects/index'
   # get 'subjects/show'
   # get 'subjects/new'
   # get 'subjects/edit'
   # get 'subjects/delete'
 
-  root 'demo#index'
 
   resources :subjects do
     member do
